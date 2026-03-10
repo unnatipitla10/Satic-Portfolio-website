485a5d7e0575bbb1bc1f25be086d0ea1b3c9ae0f
Unnati Pitla <unnatipitla1026@gmail.com>
2026-03-10 19:12:23 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,11 @@
+function toggleMenu(){
 
+let menu = document.getElementById("mobileMenu");
+
+if(menu.style.display === "flex"){
+menu.style.display="none";
+}else{
+menu.style.display="flex";
+}
+
+}
