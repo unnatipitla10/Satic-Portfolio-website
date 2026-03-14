dbe825db1105bb1acb882c6ade6fa9ed8ffee108
Unnati Pitla <unnatipitla1026@gmail.com>
2026-03-14 15:35:38 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,29 @@
+/*  Hamburger Menu*/
 function toggleMenu(){
-
-let menu = document.getElementById("mobileMenu");
-
-if(menu.style.display === "flex"){
-menu.style.display="none";
-}else{
-menu.style.display="flex";
+  
+  let menu = document.getElementById("mobileMenu");
+  
+  if(menu.style.display === "flex"){
+  menu.style.display="none";
+  }else{
+  menu.style.display="flex";
+  }
 }
 
-}
+/* Contact Section */
+const form = document.getElementById("contactForm");
+const popup = document.getElementById("successPopup");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+popup.style.display = "block";
+
+setTimeout(() => {
+popup.style.display = "none";
+},5000);
+
+form.reset();
+
+});
