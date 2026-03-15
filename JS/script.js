b1ca54e9351be66d7b84fd38dea0ac44823e48fe
Unnati Pitla <unnatipitla1026@gmail.com>
2026-03-15 15:31:44 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -27,3 +27,23 @@ popup.style.display = "none";
 form.reset();
 
 });
+
+// Go-to-top button 
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {
+
+if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+topBtn.style.display = "block";
+} else {
+topBtn.style.display = "none";
+}
+
+};
+
+function topFunction() {
+window.scrollTo({
+top: 0,
+behavior: "smooth"
+});
+}
